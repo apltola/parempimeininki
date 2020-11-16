0e9a08186039a06dfb4de0ad5beb5bcca64f8d4b
allu <aleksijaakkojuhani@gmail.com>
2020-11-16 19:44:24 +0200

    remove vercel link

--- components/footer.js
+++ components/footer.js
@@ -3,14 +3,10 @@ import styles from '../styles/Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <span>
         Powered by{' '}
         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
+      </span>
       <a
         href="https://github.com/apltola/parempimeininki"
         target="_blank"
