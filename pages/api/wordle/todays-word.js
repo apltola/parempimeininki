788d60abd5622ba27b8ee6c9402035c69e59b34b
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2022-05-21 20:27:42 +0300

    hmm

--- pages/api/wordle/todays-word.js
+++ pages/api/wordle/todays-word.js
@@ -1,5 +1,5 @@
-// import { Pool } from 'pg';
-// const pool = new Pool();
+import { Pool } from 'pg';
+const pool = new Pool();
 
 async function handler(req, res) {
   if (req.method !== 'GET') {
@@ -13,11 +13,11 @@ async function handler(req, res) {
   console.log('todays word enpoint');
   const todaysDateString = new Date().toISOString().slice(0, 10);
 
-  // const {
-  //   rows: [row],
-  // } = await pool.query(`SELECT word FROM wordle WHERE date = $1`, [
-  //   todaysDateString,
-  // ]);
+  pool
+    .query(`SELECT word FROM wordle WHERE date = $1`, [todaysDateString])
+    .then((res) => {
+      console.log(res);
+    });
 
   res.status(200).json({ word: 'hihna' });
 }
